5f489d647b0a0e20969d9892f99128358aec3ec6
soetl <soetl@proton.me>
2025-09-13 19:31:40 +0200

    system: add secure boot with lanzaboote and sbctl

--- modules/nixos/system.nix
+++ modules/nixos/system.nix
@@ -1,5 +1,6 @@
 {
   imports = [
     ./system/boot-loader.nix
+    ./system/secure-boot.nix
   ];
 }
---
+++ modules/nixos/system/secure-boot.nix
@@ -0,0 +1,29 @@
+{
+  pkgs,
+  inputs,
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.nixosModules.system.secureboot;
+in
+  with lib; {
+    imports = [
+      inputs.lanzaboote.nixosModules.lanzaboote
+    ];
+
+    options.nixosModules.system.secureboot.enable = mkEnableOption "Secure Boot via Lanzaboote and sbctl";
+
+    config = mkIf cfg.enable {
+      environment.systemPackages = [
+        pkgs.sbctl
+      ];
+
+      boot.loader.systemd-boot.enable = lib.mkForce false;
+
+      boot.lanzaboote = {
+        enable = true;
+        pkiBundle = "/var/lib/sbctl";
+      };
+    };
+  }
